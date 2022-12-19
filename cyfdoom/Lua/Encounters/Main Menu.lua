4c377056fe0afea0f80094747d988f7898815f08
KeepDrive <ipad2kirillo@gmail.com>
2022-12-19 15:31:16 +0300

    This is catastrophically bad

--- cyfdoom/Lua/Encounters/Main Menu.lua
+++
@@ -1,8 +0,0 @@
-wavetimer = math.huge -- Makes the wave effectively infinte
-enemies   = {"emptyMonster"}
-nextwaves = {"cyfdoomWADChoice"}
-
-function EncounterStarting()
-    NewAudio.Stop("src")
-    State("DEFENDING") -- Makes the wave start immediately
-end
